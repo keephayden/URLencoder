8237d31f5f6c54693291bce20c2c68e89ac05a02
Jordan Hayden <keephayden@mwc-055168.dhcp.missouri.edu>
2020-02-21 14:57:58 -0600

    Code changes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,54 @@ namespace URLencoder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string yn = "y";
+
+            while (yn == "y")
+            {
+                string project;
+                Console.WriteLine("Enter the project name: ");
+                project = Console.ReadLine();
+
+                
+                while(IsValid(project) != true)
+                {
+                    Console.WriteLine("Please enter a valid project.");
+                    Console.WriteLine("Enter the project name: ");
+                    project = Console.ReadLine();
+                }
+
+                string activity;
+                Console.WriteLine("Enter the activity name: ");
+                activity = Console.ReadLine();
+
+                while (IsValid(activity) != true)
+                {
+                    Console.WriteLine("Please enter a valid activity.");
+                    Console.WriteLine("Enter the activity name: ");
+                    activity = Console.ReadLine();
+                }
+
+                Console.WriteLine("Generating URL...\n");
+
+
+                string url = ("https://companyserver.com/content/") + project + ("/files/") + activity + "/" + activity + "report";
+                Console.WriteLine("{0}", url);
+
+
+                Console.WriteLine("Would you like to do another? type y/n: ");
+                yn = Console.ReadLine();
+            }
+        }
+        static bool IsValid(string input)
+        {
+            foreach(char character in input.ToCharArray())
+            {
+                if (character == 0x1F)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
