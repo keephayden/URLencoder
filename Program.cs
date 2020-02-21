531285c5da15a941181fbbcf1d475cb2ec5417e2
Jordan Hayden <keephayden@mwc-055168.dhcp.missouri.edu>
2020-02-21 14:49:00 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace URLencoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
